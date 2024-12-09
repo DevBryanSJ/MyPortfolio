00ab923b414ced72253ef5f713676747eb1701f9
BryanSigala <bryansigala@hotmail.com>
2024-12-09 00:46:03 -0600

    add: add social buttons in about component

--- components/common/template.jsx
+++ components/common/template.jsx
@@ -1,9 +1,26 @@
 "use client";
 
 //imports
-import { SpaceButton } from "../ui/buttons";
+import { SpaceButton, IconBtn, SocialButtons } from "../ui/buttons";
 
 const text = {
+    connect: {
+        linkedin: {
+            name: 'linkedin',
+            link: "https://www.linkedin.com/in/bryan-sigala/", 
+            icon: <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24" className="svgIcontwit"><path fill="currentColor" d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2zm-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93zM6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37z"/></svg>,
+        },
+        github: {
+            name: 'github',
+            link: "https://github.com/StellarDBryan",
+            icon: <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24" className="svgIcontwit"><path fill="currentColor" d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33s1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2"/></svg>,
+        }, 
+        x: {
+            name: 'x',
+            link: "https://x.com/BSigala16", 
+            icon: <svg xmlns="http://www.w3.org/2000/svg" width="1.6em" height="1.6em" viewBox="0 0 14 14" className="svgIcontwit"><g fill="none"><g clip-path="url(#primeTwitter0)"><path fill="currentColor" d="M11.025.656h2.147L8.482 6.03L14 13.344H9.68L6.294 8.909l-3.87 4.435H.275l5.016-5.75L0 .657h4.43L7.486 4.71zm-.755 11.4h1.19L3.78 1.877H2.504z"/></g><defs><clipPath id="primeTwitter0"><path fill="#fff" d="M0 0h14v14H0z"/></clipPath></defs></g></svg>,
+        }, 
+    },
     heroSection: {
         intro: "Hi! I'm ", 
         iam: "I'm a ",
@@ -21,6 +38,7 @@ const text = {
             p3: "I firmly believe in the power of teamwork. I enjoy helping my teammates improve their technical skills while learning from them as well. I’m also known for listening before speaking, always striving to understand problems from different perspectives to find the best possible solution.", 
             p4: "I’m inspired by geek culture and works that tell fascinating stories, which influence the way I create projects with impact and creativity. I’m always open to new opportunities and collaborations. If you have something in mind, let’s connect and make it a reality!", 
         }, 
+        connect_intro: "Connect with Me! ", 
         toolsTitle: "Work Tools ",
         workTools: {
             github: {
@@ -148,11 +166,31 @@ export function About(){
                 </div>
                 <div className="flex flex-row w-[70vw] h-auto items-center justify-around ">
                     <div className="flex flex-row justify-start items-center h-auto p-5 gap-10">
-                        <div className="flex flex-col lg:w-[55%] h-auto text-[1.05rem] font-medium text-gray-50">
+                        <div className="flex flex-col lg:w-[55%] h-auto text-[1.05rem] font-medium text-gray-50 gap-y-5">
                             <div className="w-full h-auto flex flex-col gap-y-4">
                                 {Object.values(text.about.description).map((p, index) => (
                                     <p key={index}>{p}</p>
                                 ))}
+                            </div>
+                            <div className="flex flex-row items-center justify-start gap-3">
+                                <p className="flex flex-row text-purple-300 font-semibold gap-x-3">
+                                    {text.about.connect_intro}
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                        <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                                            <path stroke-dasharray="20" stroke-dashoffset="20" d="M3 12h17.5">
+                                                <animate fill="freeze" attributeName="stroke-dashoffset" dur="0.2s" values="20;0"/>
+                                            </path>
+                                            <path stroke-dasharray="12" stroke-dashoffset="12" d="M21 12l-7 7M21 12l-7 -7">
+                                                <animate fill="freeze" attributeName="stroke-dashoffset" begin="0.2s" dur="0.2s" values="12;0"/>
+                                            </path>
+                                        </g>
+                                    </svg>
+                                </p>
+                                <div className="flex flex-row gap-0 items-center w-auto h-auto">
+                                    <IconBtn name={text.connect.linkedin.name} link={text.connect.linkedin.link} icon={text.connect.linkedin.icon} />
+                                    <IconBtn name={text.connect.github.name} link={text.connect.github.link} icon={text.connect.github.icon} />
+                                    <IconBtn name={text.connect.x.name} link={text.connect.x.link} icon={text.connect.x.icon} />
+                                </div>
                             </div>
                         </div>
                         <div className="flex flex-col w-[40%] h-full p-5  text-gray-50 ">
